093b3d10f4da1ee371be76431aa6bcd6cd989b5c
Qassandra1 <qasym42@gmail.com>
2025-03-21 15:16:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.getElementById('language-toggle').addEventListener('click', () => {
+// Language toggle
+document.getElementById("language-toggle").addEventListener("click", () => {
   if (document.documentElement.lang === "en") {
     document.documentElement.lang = "ar";
     document.body.dir = "rtl";
@@ -6,4 +7,20 @@ document.getElementById('language-toggle').addEventListener('click', () => {
     document.documentElement.lang = "en";
     document.body.dir = "ltr";
   }
+});
+
+// Scroll animation
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.animationDelay = '0s';
+      entry.target.classList.add("fade-in-visible");
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+document.querySelectorAll("section").forEach(section => {
+  observer.observe(section);
 });
